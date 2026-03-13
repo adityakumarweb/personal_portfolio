af14a87936065ead5810f42cd472c9415c8545ac
Aditya Kumar <adityapurve05@gmail.com>
2026-03-13 21:49:01 +0530

    Delete portfolio-project directory

--- portfolio-project/script.js
+++
@@ -1,9 +0,0 @@
-/* Simple script for the standalone portfolio project */
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
